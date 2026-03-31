ff2170d315f4883e26aa2ebede863f17ee187906
CatalinaBellomo <catalinabellomo05@gmail.com>
2026-03-31 01:03:15 -0300

    estructura inicial del proyecto
    Se agregan carpetas y archivos base del proyecto ReflexLab.

---
+++ src/carga_datos.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 31 00:50:30 2026
+
+@author: catalinabellomo
+"""
+
---
+++ src/metricas.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 31 00:52:40 2026
+
+@author: catalinabellomo
+"""
+
