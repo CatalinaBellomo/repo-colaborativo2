a1088ad3a357d74639d9a2605ea2380828090397
CatalinaBellomo <catalinabellomo05@gmail.com>
2026-03-31 17:49:53 -0300

    dos funciones en el archivo metricas

--- src/metricas.py
+++ src/metricas.py
@@ -8,21 +8,48 @@ Created on Tue Mar 31 00:52:40 2026
 
 def calcular_tiempo_reaccion_promedio(datos):
     """
-    Calcula el tiempo de reacción promedio.
+    Calcula el tiempo de reacción promedio de una lista de registros.
+
     Parámetros:
     - datos: list
+        Lista de registros válidos.
+
     Retorna:
     - float
+        Tiempo de reacción promedio.
     """
-    pass
+    if len(datos) == 0:
+        return 0.0
+
+    suma_tiempos = 0
+    cantidad = 0
+
+    for registro in datos:
+        suma_tiempos += registro["tiempo_reaccion"]
+        cantidad += 1
+
+    return suma_tiempos / cantidad
 
 
 def calcular_tasa_error(datos):
     """
-    Calcula la proporción de errores.
+    Calcula la proporción de respuestas incorrectas en una lista de registros.
+
     Parámetros:
     - datos: list
+        Lista de registros válidos.
+
     Retorna:
     - float
+        Tasa de error.
     """
-    pass
+    if len(datos) == 0:
+        return 0.0
+
+    errores = 0
+
+    for registro in datos:
+        if registro["resultado_respuesta"].lower() == "incorrecta":
+            errores += 1
+
+    return errores / len(datos)
