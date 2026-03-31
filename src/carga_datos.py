41e36755f919cfa33aa9c6d2ce7f76b8658be3f7
allegragegenschatz <agegenschatz@udesa.edu.ar>
2026-03-31 13:44:59 -0300

    Update carga_datos.py

--- src/carga_datos.py
+++ src/carga_datos.py
@@ -14,6 +14,11 @@ def parsear_linea(linea):
     Retorna:
     - dict
     """
+    valores = linea.strip().split(",")
+    valores[0] = int(valores[0])
+    valores[1] = int(valores[1])
+    valores[5] = float(valores[5]) if valores[5] != '' else None
+    return valores
     pass
 
 
@@ -25,6 +30,22 @@ def cargar_datos(ruta):
     Retorna:
     - list
     """
+      registro_participante = {}
+    with open(ruta, 'r') as archivo:
+        next(archivo)
+        for linea in archivo:
+            valores = parsear_linea(linea)
+            id_participante = valores[0]
+            if id_participante not in registro_participante:
+                registro_participante[id_participante] = {"id_participante": id_participante, 'trial': [],'estimulo':[], 't_inicio':[],'respuesta': [], 'tiempo_reaccion': [], 'res_respuesta': [], 'condicion':[]}
+            registro_participante[id_participante]['trial'].append(valores[1])
+            registro_participante[id_participante]['estimulo'].append(valores[2])
+            registro_participante[id_participante]['t_inicio'].append(valores[3])
+            registro_participante[id_participante]['respuesta'].append(valores[4])
+            registro_participante[id_participante]['tiempo_reaccion'].append(valores[5])
+            registro_participante[id_participante]['res_respuesta'].append(valores(6))
+            registro_participante[id_participante]['condicion'].append(valores[7])
+    return list(registro_participante.values())
     pass
 
 
