634083886d2d71f1fee8c6768cd0ba5d77d73488
mativancich <mivancich@udesa.edu.ar>
2026-04-20 13:50:06 -0300

    agrego comentarios en el archivo metricas

--- src/metricas.py
+++ src/metricas.py
@@ -24,16 +24,16 @@ def calcular_tiempo_reaccion_promedio(datos):
 
     suma_tiempos = 0
     cantidad = 0
-
+#por cada timepo de reaccion anotado, si ese timepo es mayor a 0, se agrega para ser considerado en el promedio.
     for registro in datos:
         # Solo se consideran tiempos de reacción mayores a 0, ya que los valores 0 representan ausencia de respuesta registrada y no un tiempo de reacción real.
         if registro["tiempo_reaccion"] > 0:
             suma_tiempos += registro["tiempo_reaccion"]
             cantidad += 1
-   
+#si el tiempo de reaccion es 0, devuelve 0.0 la funcion.
     if cantidad == 0:
         return 0.0
-
+#si el timpo de reaccion es distinto a 0, devuelve el promedio de todos los registros de tiempo de reaccion.
     return suma_tiempos / cantidad
 
 
@@ -49,11 +49,11 @@ def calcular_tasa_error(datos):
     - float
         Tasa de error.
     """
+#si no hay registro de datos devuelve 0.0
     if len(datos) == 0:
         return 0.0
-
     errores = 0
-
+#por cada tiempo de reaccion registrado, si contiene un error, se agrega a la variable errores.
     for registro in datos:
         if registro["resultado_respuesta"].lower() == "incorrecto":
             errores += 1
