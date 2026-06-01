556736a17c10daf1c89d03a15cd65bf95eb74df0
CatalinaBellomo <catalinabellomo05@gmail.com>
2026-06-01 01:18:55 -0300

    crear los archivos

---
+++ app.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  1 01:16:29 2026
+
+@author: catalinabellomo
+"""
+
---
+++ prompts _ dashboard.txt .py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  1 01:16:29 2026
+
+@author: catalinabellomo
+"""
